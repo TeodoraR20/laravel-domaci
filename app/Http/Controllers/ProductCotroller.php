a30f86c727ab4f1f5f8c710a2624459227129315
Teodora Radovic <tradovic19@gmail.com>
2022-03-01 23:06:12 +0100

    Login na sajt

---
+++ app/Http/Controllers/ProductCotroller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class ProductCotroller extends Controller
+{
+    //
+
+    function index()
+    {
+        return "welcome to product page";
+    }
+}
